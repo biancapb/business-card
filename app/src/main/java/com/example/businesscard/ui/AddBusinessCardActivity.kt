fa5d394257182df93007dc7ee8cf5d34deb18a38
Bianca Barbosa <yume_shiro@hotmail.com>
2021-08-12 23:33:00 -0300

    feat: imp click listener to adding card

--- app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,19 @@ package com.example.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.businesscard.App
+import com.example.businesscard.R
+import com.example.businesscard.data.BusinessCard
 import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +29,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirm.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                    name = binding.tilName.editText?.text.toString(),
+                    company = binding.tilCompanyName.editText?.text.toString(),
+                    telephone = binding.tilTelephone.editText?.text.toString(),
+                    email = binding.tilEmail.editText?.text.toString(),
+                    backgroundColor = binding.tilColor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_sucess, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
